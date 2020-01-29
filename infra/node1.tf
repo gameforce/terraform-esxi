cc389395705a948472491d5b3a89f997bd3efbaf
Nick Gotsinas <gotsinas.nick@uqam.ca>
2020-01-29 17:24:58 -0500

    remove duplicate provider

--- infra/node1.tf
+++ infra/node1.tf
@@ -1,17 +1,4 @@
 #########################################
-#  ESXI Provider host/login details
-#########################################
-#
-#   Use of variables here to hide/move the variables to a separate file
-#
-provider "esxi" {
-  esxi_hostname = var.esxi_hostname
-  esxi_hostport = var.esxi_hostport
-  esxi_username = var.esxi_username
-  esxi_password = var.esxi_password
-}
-
-#########################################
 #  ESXI Guest Node
 #########################################
   resource "esxi_guest" "default" {

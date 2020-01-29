c8b04d5cf425ee5cbb23e1fe9a30b539ffc9569c
Nick Gotsinas <gotsinas.nick@uqam.ca>
2020-01-29 18:58:14 -0500

    restructure and test with one master

---
+++ infra/node3/node3.tf
@@ -0,0 +1,38 @@
+#########################################
+#  ESXI Guest Node 3
+#########################################
+  resource "esxi_guest" "node3" {
+  guest_name = "kube-node3"
+  disk_store = "datastore-sata"
+  guestos    = "centos-64"
+
+  boot_disk_type = "thin"
+  boot_disk_size = "35"
+
+  memsize            = "2048"
+  numvcpus           = "2"
+  resource_pool_name = "/"
+  power              = "on"
+
+  clone_from_vm = "centos7"
+
+    network_interfaces {
+      virtual_network = "VM Network"
+      mac_address     = "00:50:56:a1:b1:c3"
+      nic_type        = "e1000"
+    }
+
+    connection {
+      type     = "ssh"
+      user     = "root"
+      password = var.guest_password
+      host     = "kube-node3"
+    }
+
+    provisioner "remote-exec" {
+      inline = ["hostnamectl set-hostname kube-node3","sleep 10","shutdown -r"]
+   }
+
+  guest_startup_timeout  = 45
+  guest_shutdown_timeout = 30
+}
